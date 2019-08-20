25e013135a930794397a07ec5d69fd08dd72331e
marcpassarelli <m_vpc@hotmail.com>
2019-08-20 16:11:51 -0300

    teste

--- functions/index.js
+++ functions/index.js
@@ -32,42 +32,41 @@ exports.sendMessageNotificationNewOrder = functions.database.ref('messages/{esta
     });
 });
 
-exports.sendMessageNotificationUpdateStatus = functions.database.ref('user/{userID}/details/pedidos/{messageID}/').onUpdate((snap,context) => {
-    var messageID = context.params.messageID
+exports.sendMessageNotificationUpdateStatus = functions.database.ref('messages/{estab}/{messageID}').onUpdate((snap,context) => {
+    var estab = context.params.estab
     var status = snap.after.val().status
-    var estab = snap.after.val().estabelecimento
+    var idUser = snap.after.val().idUser
     var token = snap.after.val().tokenUser
     var payload = ""
 
-      if(status==="Confirmado Recebimento"){
-        payload = {
-          notification: {
-            title: "Pedido sendo preparado para envio.",
-            body: "Assim que estiver tudo pronto com seu pedido em "+estab+" para sair para a entrega, lhe avisaremos."
-          }
-        };
-      }else if (status==="Pedido Enviado") {
-        payload = {
-          notification: {
-            title: "Pedido saindo para a entrega",
-            body: "Seu pedido realizado em "+estab+" já está com o entregador"
-          }
-        };
-      }else if (status==="Pedido Cancelado"){
-        payload = {
-          notification: {
-            title: "Pedido em "+estab+" cancelado",
-            body: "Sinto muito que seu pedido não tenha dado certo. Qualquer dúvida entre em contato com o estabelcimento."
-          }
-        };
-      }
+        if(status==="Confirmado Recebimento"){
+          payload = {
+            notification: {
+              title: "Pedido sendo preparado para envio.",
+              body: "Assim que estiver tudo pronto com seu pedido em "+estab+" para sair para a entrega, lhe avisaremos."
+            }
+          };
+        }else if (status==="Pedido Enviado") {
+          payload = {
+            notification: {
+              title: "Pedido saindo para a entrega",
+              body: "Seu pedido realizado em "+estab+" já está com o entregador"
+            }
+          };
+        }else if (status==="Pedido Cancelado"){
+          payload = {
+            notification: {
+              title: "Pedido em "+estab+" cancelado",
+              body: "Sinto muito que seu pedido não tenha dado certo. Qualquer dúvida entre em contato com o estabelcimento."
+            }
+          };
+        }
 
-      return admin.messaging().sendToDevice(token, payload)
-          .then((response)=> {
-            return null
-          })
-          .catch((error)=>{
-            return null
-          });
-
+        return admin.messaging().sendToDevice(token, payload)
+            .then((response)=> {
+              return null
+            })
+            .catch((error)=>{
+              return null
+            });
 });
